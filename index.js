e6b71f3f3b63d33f8b2ec1c6ed096cfe30b21724
Jesse Theisen <theiseja@gmail.com>
2019-03-04 20:35:38 -0600

    MVP: Project & Branch setup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server.js');
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+server.listen(9090, () => {
+  console.log('\n* Server Running on http://localhost:9090 *\n');
 });
