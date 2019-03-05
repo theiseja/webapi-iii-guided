71f598d97dd1d72e26bf5bfb305811287eac6d27
Jesse Theisen <theiseja@gmail.com>
2019-03-04 21:50:01 -0600

    MVP: Added some more custom middleware and an error handler

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 const express = require('express'); // importing a CommonJS module
+const logger = require('morgan');
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
+const parser = express.json();
+const logMiddleware = logger('dev');
+const securityMiddleware = helmet();
 
-const server = express();
+server.use(parser, logMiddleware, securityMiddleware, teamNamer, teamLogger);
 
-server.use(express.json());
+server.use('/api/hubs', restricted, hubsRouter);
 
-server.use('/api/hubs', hubsRouter);
+function teamNamer(req, res, next) {  // custom middleware function
+  req.team = "pt3";
+  next(); // can pass a value, usually use an error
+}
+
+function teamLogger(req, res, next) {
+  if (req.team) {
+    console.log('team is:', req.team);
+  }
+  next();
+}
+
+
+
+function restricted(req, res, next) {
+  const password = req.headers.authorization;
+
+  if (password === 'mellon') {
+    next();
+  } else if (password) {
+    res.status(401).json({ err: 'Invalid credentials'});
+  } else {
+    next({ err: 'no credentials provided'});
+  }
+}
 
 server.get('/', (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <p>Welcome ${req.team}, to the Lambda Hubs API</p>
     `);
+});
+
+server.use((err, req, res, next) => {  // error handler
+  res.status(400).json({ message: 'error thrown in server',
+err: err
+});
 });
 
 module.exports = server;
